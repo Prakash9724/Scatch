9fb2d88806fa086b95151611d086a1bda856004c
prakash9724 <hellobrother91069@gmail.com>
2024-09-07 14:33:27 +0530

    second commit on 7 sep 24

--- models/owner-model.js
+++ models/owner-model.js
@@ -1,7 +1,4 @@
 const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://127.0.0.1/Scatch");
-
 const ownerSchema = mongoose.Schema({
   fullname: {
     type: String,
--- routes/ownerRouter.js
+++ routes/ownerRouter.js
@@ -1,8 +1,31 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const ownerModal = require("../models/owner-model");
 
-router.get('/',function(req,res){
-    res.send("hey it's Working");
-})
+if (process.env.NODE_ENV === "development") {
+  router.post("/create", async function (req, res) {
+    let owners = await ownerModal.find();
+    if (owners.length > 1) {
+      return res
+        .send(503)
+        .send("You don't have permission to create new owner");
+    }
+
+    let { fullname, email, password } = req.body;
+
+    let createdOwner = await ownerModal.create({
+      fullname,
+      email,
+      password,
+    });
+    res.status(201).send(createdOwner);
+    
+    
+  });
+}
+
+router.get("/", function (req, res) {
+  res.send("hey it's Working");
+});
 
 module.exports = router;
